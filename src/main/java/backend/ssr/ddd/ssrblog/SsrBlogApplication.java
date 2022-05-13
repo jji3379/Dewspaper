c1f0698f2f26be9fe1656994d5f9b34923e51a11
merlinsbeard3031 <merlinsbeard3031@gmail.com>
2022-05-13 18:45:08 +0900

    init

---
+++ src/main/java/backend/ssr/ddd/ssrblog/SsrBlogApplication.java
@@ -0,0 +1,13 @@
+package backend.ssr.ddd.ssrblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsrBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/ssr/ddd/ssrblog/SsrBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.ssr.ddd.ssrblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
