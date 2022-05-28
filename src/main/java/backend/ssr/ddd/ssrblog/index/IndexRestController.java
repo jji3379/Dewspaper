9896737e6929127847835f67e336ce9176e7dc81
merlinsbeard3031 <merlinsbeard3031@gmail.com>
2022-05-28 23:37:21 +0900

    deploy

--- src/main/java/backend/ssr/ddd/ssrblog/index/IndexRestController.java
+++ src/main/java/backend/ssr/ddd/ssrblog/index/IndexRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexRestController {
 
+
     @GetMapping("/")
     public String main() throws Exception{
         return "test";
