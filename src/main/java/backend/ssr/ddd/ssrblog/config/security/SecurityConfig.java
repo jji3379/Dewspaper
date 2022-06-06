65b8c3e0d869205f59bac6f8fb9a2dfcd4af8552
jji3379 <jji3379@naver.com>
2022-06-07 01:19:11 +0900

    Feat : jwt 토큰 발급 추가

---
+++ src/main/java/backend/ssr/ddd/ssrblog/config/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package backend.ssr.ddd.ssrblog.config.security;
+
+import backend.ssr.ddd.ssrblog.filter.JwtAuthenticationFilter;
+import backend.ssr.ddd.ssrblog.oauth.jwt.JwtAccessDeniedHandler;
+import backend.ssr.ddd.ssrblog.oauth.jwt.JwtAuthenticationEntryPoint;
+import backend.ssr.ddd.ssrblog.oauth.service.CustomOauth2UserService;
+import backend.ssr.ddd.ssrblog.oauth.jwt.JwtTokenProvider;
+import backend.ssr.ddd.ssrblog.oauth.successHandler.OAuth2AuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOauth2UserService customOauth2UserService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 X
+                .and()
+                    .exceptionHandling()
+                        //.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+                    .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
+                        .antMatchers("/api/**").authenticated()
+                        .antMatchers(HttpMethod.POST).authenticated() // 등록에 대한 Method 에 접근시 인증된 사용자만 접근 가능
+                        .antMatchers(HttpMethod.PUT).authenticated() // 수정에 대한 Method 에 접근시 인증된 사용자만 접근 가능
+                        .antMatchers(HttpMethod.DELETE).authenticated() // 삭제에 대한 Method 에 접근시 인증된 사용자만 접근 가능
+                .and()
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                        .oauth2Login() // OAuth2 로그인 기능에 대한 여러 설정의 진입점
+
+                .defaultSuccessUrl("/login-success") // oauth2 인증이 성공했을 때, 이동되는 url 설정
+                .successHandler(oAuth2AuthenticationSuccessHandler) // 인증 프로세스에 따라 사용자 정의 로직 실행
+                    .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당한다.
+                        .userService(customOauth2UserService); // 로그인이 성공하면 해당 유저의 정보를 들고, customOAuth2UserService 에서 후처리를 해주겠다는 의미
+    }
+}
---
+++ src/main/java/backend/ssr/ddd/ssrblog/oauth/jwt/JwtTokenProvider.java
@@ -0,0 +1,83 @@
+package backend.ssr.ddd.ssrblog.oauth.jwt;
+
+import backend.ssr.ddd.ssrblog.oauth.service.CustomUserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+    @Value("spring.jwt.secret")
+    private String secretKey ;
+
+    private final CustomUserDetailService customUserDetailService;
+
+    private long tokenValidMilisecond = 1000L * 60 * 60; // 1시간만 토큰 유효
+
+    @PostConstruct
+    protected void init() {
+        secretKey  = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // Jwt 토큰 생성
+    public String createToken(String email, String roles, String platform) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims) // 데이터
+                .setIssuer(platform) // 발급한 플랫폼
+                .setIssuedAt(now) // 토큰 발행일자
+                .setExpiration(new Date(now.getTime() + tokenValidMilisecond)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey ) // 암호화 알고리즘, secret값 세팅
+                .compact();
+    }
+
+    // JWT 토큰에서 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(this.getUserEmail(token), this.getUserPlatform(token));
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // 토큰에서 회원 정보의 플랫폼
+    public String getUserPlatform(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getIssuer();
+    }
+
+    // Request의 Header에서 token 값을 가져옵니다. "authorization" : "token'
+    public String resolveToken(HttpServletRequest request) {
+        if(request.getHeader("authorization") != null )
+            return request.getHeader("authorization").substring(7);
+        return null;
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
