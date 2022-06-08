97c11da658c0f06f771c160fb1c1e668a91d71e4
jji3379 <jji3379@naver.com>
2022-06-09 00:36:23 +0900

    fix : index controller swagger 제외

--- src/main/java/backend/ssr/ddd/ssrblog/index/IndexController.java
+++ src/main/java/backend/ssr/ddd/ssrblog/index/IndexController.java
@@ -2,8 +2,10 @@ package backend.ssr.ddd.ssrblog.index;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
+@ApiIgnore
 public class IndexController {
 
     @GetMapping("/")
