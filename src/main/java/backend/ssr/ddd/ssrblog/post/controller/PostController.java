45e2970cc8b2c8a0f7fff49c105887b3df869d8c
jji3379 <jji3379@naver.com>
2022-06-10 01:55:58 +0900

    refactor : ResponseEntity 적용

--- src/main/java/backend/ssr/ddd/ssrblog/post/controller/PostController.java
+++ src/main/java/backend/ssr/ddd/ssrblog/post/controller/PostController.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,48 +29,48 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts") @ApiOperation(value = "게시물 전체 정보 조회", notes = "게시물 전체를 페이징 처리하여 조회한다. (공개 되어있으면서 (privated = N), 삭제 되지 않은 게시물들만 조회 (deleted = N))")
-    public Page<PostResponse> getPostPagingList(Pageable pageable) {
+    public ResponseEntity<Page<PostResponse>> getPostPagingList(Pageable pageable) {
         List<Post> postList = postService.getPostList(pageable);
         List<PostResponse> list = new ArrayList();
 
         for (Post post : postList) {
-            PostResponse response = new PostResponse();
             list.add(post.toResponse());
         }
 
-
-        return new PageImpl<>(list, pageable, list.size());
+        return new ResponseEntity(new PageImpl<>(list, pageable, list.size()), HttpStatus.OK);
     }
 
     @GetMapping("/post/{postIdx}") @ApiOperation(value = "게시물 조회", notes = "게시물 번호가 postIdx 인 게시물의 정보를 조회한다.")
     @ApiImplicitParam(name = "postIdx", required = true, value = "예 : 1")
-    public PostResponse getPost(@PathVariable Long postIdx) {
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long postIdx) {
         Post post = postService.getPost(postIdx);
 
-        return post.toResponse();
+        return new ResponseEntity(post.toResponse(), HttpStatus.OK);
     }
 
     @PostMapping("/post") @ApiOperation(value = "게시물 등록", notes = "postRequest 를 받아 새로운 게시물을 등록한다.")
-    public PostResponse savePost(@RequestBody @Valid PostRequest postRequest, Errors errors) {
-        Post post = postService.savePost(postRequest);
-
+    public ResponseEntity<PostResponse> savePost(@RequestBody @Valid PostRequest postRequest, Errors errors) {
         if (errors.hasErrors()) {
-            throw new IllegalArgumentException(errors.getFieldError().getDefaultMessage());
+
+            return new ResponseEntity(errors.getFieldError().getDefaultMessage(),HttpStatus.BAD_REQUEST);
         }
 
-        return post.toResponse();
+        Post post = postService.savePost(postRequest);
+
+        return new ResponseEntity(post.toResponse(), HttpStatus.CREATED);
     }
 
     @PutMapping("/post/{postIdx}") @ApiOperation(value = "게시물 정보 수정", notes = "게시물 번호가 postIdx 인 게시물을 postRequest 를 받아 게시물의 정보를 수정한다.")
     @ApiImplicitParam(name = "postIdx", required = true, value = "예 : 1")
-    public PostResponse updatePost(@PathVariable Long postIdx, @RequestBody @Valid PostRequest postRequest, Errors errors) {
-        Post post = postService.updatePost(postIdx, postRequest);
-
+    public ResponseEntity<PostResponse> updatePost(@PathVariable Long postIdx, @RequestBody @Valid PostRequest postRequest, Errors errors) {
         if (errors.hasErrors()) {
-            throw new IllegalArgumentException(errors.getFieldError().getDefaultMessage());
+
+            return new ResponseEntity(errors.getFieldError().getDefaultMessage(),HttpStatus.BAD_REQUEST);
         }
 
-        return post.toResponse();
+        Post post = postService.updatePost(postIdx, postRequest);
+
+        return new ResponseEntity(post.toResponse(), HttpStatus.OK);
     }
 
     @DeleteMapping("/post/{postIdx}") @ApiOperation(value = "게시물 삭제", notes = "게시물 번호가 postIdx 인 게시물을 삭제한다.")
