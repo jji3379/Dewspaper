29461ae1f55ba68e4cf9050664660a1cb10902fb
jji3379 <jji3379@naver.com>
2022-06-11 00:21:33 +0900

    test : success redirect url 테스트

--- src/main/java/backend/ssr/ddd/ssrblog/oauth/successHandler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/backend/ssr/ddd/ssrblog/oauth/successHandler/OAuth2AuthenticationSuccessHandler.java
@@ -35,7 +35,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
         log.info("jwt : {}", jwt);
 
-        String url = makeRedirectUrl(jwt);
+        String url = makeRedirectUrl(request, jwt);
 
         if (response.isCommitted()) {
             logger.debug("응답이 이미 커밋된 상태입니다. " + url + "로 리다이렉트하도록 바꿀 수 없습니다.");
@@ -44,8 +44,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         getRedirectStrategy().sendRedirect(request, response, url);
     }
 
-    private String makeRedirectUrl(String token) {
-        return UriComponentsBuilder.fromUriString("https://jbs7uh.csb.app/oauth2/redirect/"+token)
+    private String makeRedirectUrl(HttpServletRequest request, String token) {
+        return UriComponentsBuilder.fromUriString("https://jbs7uh.csb.app/"+request.getRequestURI()+token)
                 .build().toUriString();
     }
 }
