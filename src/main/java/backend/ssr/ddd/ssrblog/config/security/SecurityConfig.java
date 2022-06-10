bfe7aa14a5c6810bba8592c837194c5edce7398c
jji3379 <jji3379@naver.com>
2022-06-11 01:26:06 +0900

    feat : cors 추가

--- src/main/java/backend/ssr/ddd/ssrblog/config/security/SecurityConfig.java
+++ src/main/java/backend/ssr/ddd/ssrblog/config/security/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 X
                 .and()
