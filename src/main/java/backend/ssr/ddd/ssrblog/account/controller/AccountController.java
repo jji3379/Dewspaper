283aacbba88fb18a44bc91c6d90f4b628f7528a9
jji3379 <jji3379@naver.com>
2022-06-21 16:03:28 +0900

    fix : test api 경로 수정

--- src/main/java/backend/ssr/ddd/ssrblog/account/controller/AccountController.java
+++ src/main/java/backend/ssr/ddd/ssrblog/account/controller/AccountController.java
@@ -12,18 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @Api(tags = "1. 회원 API")
+@RequestMapping("/api")
 public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("/api/{accountIdx}") @ApiOperation(value = "회원 정보 조회", notes = "회원의 상세 정보를 조회 한다.")
+    @GetMapping("/{accountIdx}") @ApiOperation(value = "회원 정보 조회", notes = "회원의 상세 정보를 조회 한다.")
     @ApiImplicitParam(name = "accountIdx", required = true, value = "예 : 1")
     public AccountResponse getAccount(@PathVariable Long accountIdx) {
 
         return accountService.getAccountOne(accountIdx).toResponse();
     }
 
-    @PostMapping("/api/refresh-token") @ApiOperation(value = "토큰 재발급", notes = "refresh-token 을 header 에 입력하여 새로운 토큰을 발급 받는다.")
+    @PostMapping("/refresh-token") @ApiOperation(value = "토큰 재발급", notes = "refresh-token 을 header 에 입력하여 새로운 토큰을 발급 받는다.")
     @ApiImplicitParam(name = "refresh-token", value = "예 : eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqamkzMzc5QG5hdmVyLmNvbSIsImlzcyI6Im5hdmVyIiwiaWF0IjoxNjU1Mzk0OTY4LCJleHAiOjE2NTU5OTk3Njh9.j-HAHp05U6WG20dH9gVtqB64e5TAB7ECliqEhuVVCcM", required = true, dataType = "String", paramType = "header")
     public String getRefreshToken(@RequestHeader(value="refresh-token") String refreshToken) {
 
