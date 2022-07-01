f3ff4fa38d04bf4a0f5952aea48431525262c492
jji3379 <jji3379@naver.com>
2022-07-02 03:30:52 +0900

    fix : writer delYn 추가

--- src/main/java/backend/ssr/ddd/ssrblog/writer/domain/entity/Writer.java
+++ src/main/java/backend/ssr/ddd/ssrblog/writer/domain/entity/Writer.java
@@ -1,33 +1,45 @@
 package backend.ssr.ddd.ssrblog.writer.domain.entity;
 
 import backend.ssr.ddd.ssrblog.account.domain.entity.Account;
+import backend.ssr.ddd.ssrblog.common.TimeEntity.BaseTimeEntity;
 import backend.ssr.ddd.ssrblog.post.domain.entity.Post;
+import backend.ssr.ddd.ssrblog.writer.dto.WriterRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @IdClass(WriterId.class) // 복합키 식별자 클래스
-public class Writer {
+public class Writer extends BaseTimeEntity {
 
     @Id
-    private Long postIdx;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post postIdx;
 
     @Id
-    private Long accountIdx;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account accountIdx;
 
     private Long realWriter;
 
+    private String delYn = "N";
+
     @Builder
-    public Writer(Long postIdx, Long accountIdx, Long realWriter) {
+    public Writer(Post postIdx, Account accountIdx, Long realWriter, String delYn) {
         this.postIdx = postIdx;
         this.accountIdx = accountIdx;
         this.realWriter = realWriter;
+        this.delYn = delYn;
+    }
+
+    public Writer add(Post postIdx, WriterRequest account) {
+        this.postIdx = postIdx;
+        this.accountIdx = Account.builder().accountIdx(account.getAccountIdx()).build();
+
+        return this;
     }
 }
